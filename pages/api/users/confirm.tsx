ff9b16a0e350d72d9b0512762341670fc84c57d8
jongwonp <jw032876@gmail.com>
2023-05-14 20:46:38 +0900

    Feat: iron-session 설치, 로그인 시 올바른 토큰을  입력받으면 세션에 userId 저장하는 기능 추가

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,4 +1,5 @@
 import client from '@/libs/server/client';
+import { withIronSessionApiRoute } from 'iron-session/next';
 import withHandler, { ResponseType } from '@/libs/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
 
@@ -7,8 +8,20 @@ async function handler(
   res: NextApiResponse<ResponseType>
 ) {
   const { token } = req.body;
-  console.log(token);
+  const exist = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+  if (!exist) res.status(404).end();
+  req.session.user = {
+    id: exist?.userId,
+  };
+  await req.session.save();
   res.status(200).end();
 }
 
-export default withHandler('POST', handler);
+export default withIronSessionApiRoute(withHandler('POST', handler), {
+  cookieName: 'carrotsession',
+  password: '1232450294058402981231243235253245643669346214124',
+});
