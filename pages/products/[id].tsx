4cbe23b784d5fc9671b70fd2988642612a859736
jongwonp <jw032876@gmail.com>
2023-05-17 18:06:54 +0900

    Feat: 관련있는 물품 필터링 기능, 해당 데이터 전달 기능 구현

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,13 +1,24 @@
 import Button from '@/components/button';
 import Layout from '@/components/layouts';
+import { Product, User } from '@prisma/client';
 import type { NextPage } from 'next';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 
+interface ProductWithUser extends Product {
+  user: User;
+}
+
+interface ItemDatilResponse {
+  ok: boolean;
+  product: ProductWithUser;
+  relatedProducts: Product[];
+}
+
 const ItemDetail: NextPage = () => {
   const router = useRouter();
-  const { data } = useSWR(
+  const { data } = useSWR<ItemDatilResponse>(
     router.query.id ? `/api/products/${router.query.id}` : null
   );
   console.log('data', data);
@@ -71,12 +82,16 @@ const ItemDetail: NextPage = () => {
         <div>
           <h2 className="text-2xl font-bold text-gray-900">Similar items</h2>
           <div className="grid grid-cols-2 gap-4">
-            {[1, 2, 3, 4, 5, 6].map((_, i) => (
-              <div key={i} className="">
-                <div className=" h-56 w-full mb-4 bg-slate-300" />
-                <h3 className="text-gray-700 -mb-1">Galaxy S60</h3>
-                <p className="text-sm font-medium text-gray-900">$6</p>
-              </div>
+            {data?.relatedProducts.map((product) => (
+              <Link href={`/products/${product.id}`}>
+                <div key={product.id} className="">
+                  <div className=" h-56 w-full mb-4 bg-slate-300" />
+                  <h3 className="text-gray-700 -mb-1">{product.name}</h3>
+                  <p className="text-sm font-medium text-gray-900">
+                    {product.price}
+                  </p>
+                </div>
+              </Link>
             ))}
           </div>
         </div>
