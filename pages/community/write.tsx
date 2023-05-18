3dfa9f6e3b2e452c6fbb3117ec1bc872cb872ce7
jongwonp <jw032876@gmail.com>
2023-05-18 20:03:22 +0900

    Feat: 질문 글 올리는 페이지의 form으로 데이터 서버로 보내는 기능 구현

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -1,14 +1,44 @@
 import Button from '@/components/button';
 import Layout from '@/components/layouts';
 import TextArea from '@/components/textarea';
+import useMutation from '@/libs/client/useMutation';
+import { Post } from '@prisma/client';
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+
+interface WriteForm {
+  question: string;
+}
+
+interface WriteResponse {
+  ok: boolean;
+  post: Post;
+}
 
 const Write: NextPage = () => {
+  const router = useRouter();
+  const { register, handleSubmit } = useForm<WriteForm>();
+  const [post, { loading, data }] = useMutation<WriteResponse>('/api/posts');
+  const onValid = (data: WriteForm) => {
+    if (loading) return;
+    post(data);
+  };
+  useEffect(() => {
+    if (data && data.ok) {
+      router.push(`/community/${data.post.id}`);
+    }
+  }, [data]);
   return (
     <Layout canGoBack>
-      <form className="px-4 py-10">
-        <TextArea placeholder="Ask a question!" required />
-        <Button text="Submit" />
+      <form onSubmit={handleSubmit(onValid)} className="px-4 py-10">
+        <TextArea
+          register={register('question', { required: true, minLength: 5 })}
+          placeholder="Ask a question!"
+          required
+        />
+        <Button text={loading ? 'Loading...' : 'Submit'} />
       </form>
     </Layout>
   );
