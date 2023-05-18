e0dfb6f238e4136f9efd2b7d45778d5944a1b358
jongwonp <jw032876@gmail.com>
2023-05-18 20:31:29 +0900

    Feat: 질문글을 보기 위한 데이터 GET 요청용 API handler 함수 구현

---
+++ pages/api/posts/[id]/index.ts
@@ -0,0 +1,66 @@
+import client from '@/libs/server/client';
+import withHandler, { ResponseType } from '@/libs/server/withHandler';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { withApiSession } from '@/libs/server/withSession';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+    session: { user },
+  } = req;
+  const post = await client.post.findUnique({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+      answers: {
+        select: {
+          answer: true,
+          id: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+              avatar: true,
+            },
+          },
+        },
+      },
+      _count: {
+        select: {
+          answers: true,
+          wondering: true,
+        },
+      },
+    },
+  });
+  const isWondering = Boolean(
+    await client.wondering.findFirst({
+      where: {
+        postId: Number(id),
+        userId: user?.id,
+      },
+      select: {
+        id: true,
+      },
+    })
+  );
+  if (!post) res.status(404).json({ ok: false, error: 'Not found post' });
+  res.json({
+    ok: true,
+    post,
+    isWondering,
+  });
+}
+
+export default withApiSession(withHandler({ methods: ['GET'], handler }));
