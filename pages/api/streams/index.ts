40fb190329132dca5818a7f92a757919e314ce6f
jw032876@gmail.com <jw032876@gmail.com>
2023-08-11 19:48:38 +0900

    Feat: NCP의 Live Station 서비스에서 채널을 생성하도록 요청하는 기능 및 생성된 채널의 정보를 가져오는 기능 추가

--- pages/api/streams/index.ts
+++ pages/api/streams/index.ts
@@ -16,8 +16,62 @@ async function handler(
     const streams = await client.stream.findMany();
     res.json({ ok: true, streams });
   } else if (req.method === "POST") {
+    const timestamp = String(Date.now());
+    //** NCP Live Station 채널을 만드는 요청을 한 뒤, 채널의 ID를 응답으로 받아옴 */
+    const {
+      content: { channelId },
+    } = await fetch(`https://livestation.apigw.ntruss.com/api/v2/channels`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-ncp-apigw-timestamp": timestamp,
+        "x-ncp-iam-access-key": `${process.env.NAVER_CLOUD_API_ACCESS_KEY}`,
+        "x-ncp-apigw-signature-v2": `${makeSignature(
+          timestamp,
+          "POST",
+          "/api/v2/channels"
+        )}`,
+        "x-ncp-region_code": "KR",
+      },
+      body: JSON.stringify({
+        channelName: `${name}`,
+        cdn: {
+          createCdn: true,
+          cdnType: "CDN_PLUS",
+        },
+        qualitySetId: 3,
+        useDvr: false,
+        record: {
+          type: "MANUAL_UPLOAD",
+        },
+      }),
+    }).then((data) => data.json());
+
+    //* NCP Live Station 채널 정보를 요청한 뒤, 채널의 key와 url을 응답으로 받아옴 */
+    const {
+      content: { streamKey, publishUrl },
+    } = await fetch(
+      `https://livestation.apigw.ntruss.com/api/v2/channels/${channelId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-ncp-apigw-timestamp": timestamp,
+          "x-ncp-iam-access-key": `${process.env.NAVER_CLOUD_API_ACCESS_KEY}`,
+          "x-ncp-apigw-signature-v2": `${makeSignature(
+            timestamp,
+            "GET",
+            `/api/v2/channels/${channelId}`
+          )}`,
+          "x-ncp-region_code": "KR",
+        },
+      }
+    ).then((data) => data.json());
     const stream = await client.stream.create({
       data: {
+        ncpLiveId: channelId,
+        ncpLiveKey: streamKey,
+        ncpLiveUrl: publishUrl,
         name,
         price,
         description,
@@ -29,42 +83,15 @@ async function handler(
       },
     });
 
-    const timestamp = String(Date.now());
-    const liveStationRes = await fetch(
-      `https://livestation.apigw.ntruss.com/api/v2/channels`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "x-ncp-apigw-timestamp": timestamp,
-          "x-ncp-iam-access-key": `${process.env.NAVER_CLOUD_API_ACCESS_KEY}`,
-          "x-ncp-apigw-signature-v2": `${makeSignature(timestamp)}`,
-          "x-ncp-region_code": "KR",
-        },
-        body: JSON.stringify({
-          channelName: `${name}`,
-          cdn: {
-            createCdn: true,
-            cdnType: "CDN_PLUS",
-          },
-          qualitySetId: 3,
-          useDvr: false,
-          record: {
-            type: "MANUAL_UPLOAD",
-          },
-        }),
-      }
-    ).then((data) => data.json());
-    console.log(liveStationRes);
     res.json({ ok: true, stream });
   }
 }
 
-function makeSignature(ts: string) {
+function makeSignature(ts: string, method: string, url: string) {
   var space = " "; // one space
   var newLine = "\n"; // new line
-  var method = "POST"; // method
-  var url = `/api/v2/channels`; // url (include query string)
+  var method = method; // method
+  var url = url; // url (include query string)
   var timestamp = ts; // current timestamp (epoch)
   var accessKey = `${process.env.NAVER_CLOUD_API_ACCESS_KEY}`; // access key id (from portal or Sub Account)
   var secretKey = `${process.env.NAVER_CLOUD_API_SECRET_KEY}`; // secret key (from portal or Sub Account)
