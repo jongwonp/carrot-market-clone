845d2cd5d776a8ca965462e31a94a594ca528a92
jw032876@gmail.com <jw032876@gmail.com>
2023-08-11 20:05:25 +0900

    Feat: 라이브 상세 페이지에 해당 페이지에 해당하는 Live Station의 채널 정보를 가져오는 기능 추가

--- pages/api/streams/[id]/index.ts
+++ pages/api/streams/[id]/index.ts
@@ -1,7 +1,8 @@
-import client from '@/libs/server/client';
-import withHandler, { ResponseType } from '@/libs/server/withHandler';
-import { NextApiRequest, NextApiResponse } from 'next';
-import { withApiSession } from '@/libs/server/withSession';
+import client from "@/libs/server/client";
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@/libs/server/withSession";
+import CryptoJS from "crypto-js";
 
 async function handler(
   req: NextApiRequest,
@@ -9,6 +10,7 @@ async function handler(
 ) {
   const {
     query: { id },
+    session: { user },
   } = req;
   const stream = await client.stream.findUnique({
     where: {
@@ -29,7 +31,55 @@ async function handler(
       },
     },
   });
-  res.json({ ok: true, stream });
+
+  const timestamp = String(Date.now());
+  const { content } = await fetch(
+    `https://livestation.apigw.ntruss.com/api/v2/channels/${stream?.ncpLiveId}/serviceUrls?serviceUrlType=GENERAL`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "x-ncp-apigw-timestamp": timestamp,
+        "x-ncp-iam-access-key": `${process.env.NAVER_CLOUD_API_ACCESS_KEY}`,
+        "x-ncp-apigw-signature-v2": `${makeSignature(
+          timestamp,
+          "GET",
+          `/api/v2/channels/${stream?.ncpLiveId}/serviceUrls?serviceUrlType=GENERAL`
+        )}`,
+        "x-ncp-region_code": "KR",
+      },
+    }
+  ).then((data) => data.json());
+
+  const isOwner = stream?.userId === user?.id;
+  if (stream && !isOwner) {
+    stream.ncpLiveKey = "xxxxx";
+    stream.ncpLiveUrl = "xxxxx";
+  }
+  res.json({ ok: true, stream, content });
 }
 
-export default withApiSession(withHandler({ methods: ['GET'], handler }));
+function makeSignature(ts: string, method: string, url: string) {
+  var space = " "; // one space
+  var newLine = "\n"; // new line
+  var method = method; // method
+  var url = url; // url (include query string)
+  var timestamp = ts; // current timestamp (epoch)
+  var accessKey = `${process.env.NAVER_CLOUD_API_ACCESS_KEY}`; // access key id (from portal or Sub Account)
+  var secretKey = `${process.env.NAVER_CLOUD_API_SECRET_KEY}`; // secret key (from portal or Sub Account)
+
+  var hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secretKey);
+  hmac.update(method);
+  hmac.update(space);
+  hmac.update(url);
+  hmac.update(newLine);
+  hmac.update(timestamp);
+  hmac.update(newLine);
+  hmac.update(accessKey);
+
+  var hash = hmac.finalize();
+
+  return hash.toString(CryptoJS.enc.Base64);
+}
+
+export default withApiSession(withHandler({ methods: ["GET"], handler }));
